702b602f237e7d570e2f86d3759402894fda07d5
Remko Duursma <remkoduursma@gmail.com>
2018-01-12 12:18:57 +1100

    completed draft without notes or dangles

--- run.R
+++ run.R
@@ -3,7 +3,8 @@
 source("R/load.R")
 
 # Make figures as PDF (see output/)
-# Conversion to png relies on an installation of ImageMagick (and GhostScript); may fail on your system.
+# Conversion to png relies on an installation of ImageMagick (and GhostScript),
+# may fail on your system (but you can ignore these errors and continue).
 source("R/make_figures.R")
 
 # Convert manuscript (output is manuscript.docx & manuscript_suppinfo.docx).
